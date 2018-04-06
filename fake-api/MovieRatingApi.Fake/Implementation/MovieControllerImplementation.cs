b191e18e7263a55e712d942c9eeb7a92f7cde1d0
Nuno Caneco <nuno.caneco@gmail.com>
2018-04-06 01:15:40 +0100

    Add fake-api

---
+++ fake-api/MovieRatingApi.Fake/Implementation/MovieControllerImplementation.cs
@@ -0,0 +1,40 @@
+﻿using MovieRatingApi.Fake.Repository;
+using MovieRatingAPI.Fake;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRatingApi.Fake.Implementation
+{
+    public class MovieControllerImplementation : IMovieController
+    {
+        private readonly MoviesRepository moviesRepository;
+
+        public MovieControllerImplementation(MoviesRepository moviesRepository)
+        {
+            this.moviesRepository = moviesRepository ?? throw new ArgumentNullException(nameof(moviesRepository));
+        }
+
+        public Task<MovieData> GetAsync(string id)
+        {
+            return Task.FromResult(moviesRepository.GetByImdbId(id));
+        }
+
+        public Task<List<MovieEntry>> SearchAsync(string q)
+        {
+            var movies = moviesRepository.Search(q)
+                .Select(m => new MovieEntry
+                {
+                    ImdbID = m.ImdbID,
+                    Poster = m.Poster,
+                    Title = m.Title,
+                    Type = "movie",
+                    Year = m.Year
+                });
+
+            return Task.FromResult(movies.ToList());
+        }
+    }
+}
---
+++ fake-api/MovieRatingApi.Fake/Repository/MoviesRepository.cs
@@ -0,0 +1,45 @@
+﻿using MovieRatingAPI.Fake;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MovieRatingApi.Fake.Repository
+{
+    public class MoviesRepository
+    {
+        private readonly static List<MovieData> movies = BuildMovies();
+
+        public IEnumerable<MovieData> Search(string queryString) => movies.Where(m => m.Title.Contains(queryString) || m.Director.Contains(queryString));
+
+        public MovieData GetByImdbId(string imdbId) => movies.FirstOrDefault(m => string.Equals(m.ImdbID, imdbId, StringComparison.OrdinalIgnoreCase));
+
+        private static List<MovieData> BuildMovies() =>
+            new List<MovieData>
+            {
+                new MovieData
+                {
+                    ImdbID =  "tt0093870",
+                    Title = "RoboCop",
+                    Year = "1987",
+                    Director = "Paul Verhoeven",
+                    Poster=  "https://ia.media-imdb.com/images/M/MV5BZWVlYzU2ZjQtZmNkMi00OTc3LTkwZmYtZDVjNmY4OWFmZGJlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+                },
+                new MovieData
+                {
+                    ImdbID =  "tt1234721",
+                    Title = "RoboCop",
+                    Year = "2014",
+                    Director = "Paul Verhoeven",
+                    Poster=  "https://images-na.ssl-images-amazon.com/images/M/MV5BMjAyOTUzMTcxN15BMl5BanBnXkFtZTgwMjkyOTc1MDE@._V1_SX300.jpg"
+                },
+                new MovieData
+                {
+                    ImdbID =  "tt0100502",
+                    Title = "RoboCop 2",
+                    Year = "1990",
+                    Director = "Paul Verhoeven",
+                    Poster=  "https://ia.media-imdb.com/images/M/MV5BNGQ5ZjI3YzYtNjM2Ni00MTFjLWEyOWMtOTEwNjI5MThlZTlmXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+                }
+            };
+    }
+}
