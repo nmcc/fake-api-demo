a64e63eae3a02dd2b9e62f57576b19cb3727dc20
Nuno Caneco <nuno.caneco@gmail.com>
2018-04-07 00:46:21 +0100

    Add configuration parameters for the several API endpoints

--- ui/MovieRating.UI/Program.cs
+++ ui/MovieRating.UI/Program.cs
@@ -20,6 +20,7 @@ namespace MovieRating.UI
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls("http://localhost:10000/")
                 .Build();
     }
 }
