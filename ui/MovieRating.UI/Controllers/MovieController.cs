30fae367a92467e4ee1ad9d79ac0213cf1edce0d
Nuno Caneco <nuno.caneco@gmail.com>
2018-04-06 01:16:07 +0100

    Add initial version of UI application

---
+++ ui/MovieRating.UI/Controllers/MovieController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieRating.UI.MovieRatingApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRating.UI.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly MovieClient movieClient;
+
+        public MovieController(MovieClient movieClient)
+        {
+            this.movieClient = movieClient ?? throw new ArgumentNullException(nameof(movieClient));
+        }
+
+        public IActionResult Search(string q)
+        {
+            var result = this.movieClient.SearchAsync(q);
+
+            return View(result);
+        }
+    }
+}
---
+++ ui/MovieRating.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MovieRating.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
